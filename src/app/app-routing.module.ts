4b7d3224a3b9273ec9feaa81dd6d4349e5be4b12
Johnny Avakian <javak@yahoo.com>
2019-06-12 00:48:15 -0700

    inital_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PinfoComponent } from './components/pinfo/pinfo.component';
+import { CinfoComponent } from './components/cinfo/cinfo.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { SuccessComponent } from './components/success/success.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"pinfo",component:PinfoComponent},
+{path:"cinfo",component:CinfoComponent},
+{path:"skills",component:SkillsComponent},
+{path:"jobs",component:JobsComponent},
+{path:"success",component:SuccessComponent},
+{path:"",redirectTo:"/pinfo",pathMatch:"full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PinfoComponent } from './components/pinfo/pinfo.component';
+import { CinfoComponent } from './components/cinfo/cinfo.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { SuccessComponent } from './components/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PinfoComponent,
+    CinfoComponent,
+    SkillsComponent,
+    JobsComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
