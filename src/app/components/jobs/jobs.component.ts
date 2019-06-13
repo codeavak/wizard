6a18455eb59e0759fcd211acff479c7c7a414d67
Johnny Avakian <javak@yahoo.com>
2019-06-12 23:21:29 -0700

    second_commit

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs.component.css']
 })
 export class JobsComponent implements OnInit {
-
+  Skills: any[];
+  onSkillTxtBlur;
+  addSkill;
+  removeSkill;
   constructor() { }
 
   ngOnInit() {
+    function checkDuplicateInObject(propertyName, inputArray) {
+      var seenDuplicate = false,
+          testObject = {};
+    
+      inputArray.map(function(item) {
+        var itemPropertyName = item[propertyName].toLowerCase();    
+        if (itemPropertyName.toLowerCase() in testObject) {
+          testObject[itemPropertyName].duplicate = true;
+          item.duplicate = true;
+          seenDuplicate = true;
+        }
+        else {
+          testObject[itemPropertyName] = item;
+          delete item.duplicate;
+        }
+      });
+    
+      return seenDuplicate;
+    }
+
+    this.removeSkill=function(index){
+      console.log(`now removing ${index}`)
+      this.Skills.splice(index,1);
+    }
+    this.addSkill=function(){
+      let count=this.Skills.length+1;
+      this.Skills.push({'skill':'job '+count});
+    }
+this.Skills=[{'skill':'job 1'},{'skill':'job 2'},{'skill':'job 3'}]
+    this.onSkillTxtBlur=function(){
+checkDuplicateInObject('skill',this.Skills);
+
+    }
   }
 
 }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+Skills: any[];
+onSkillTxtBlur
 
   constructor() { }
 
   ngOnInit() {
+    function checkDuplicateInObject(propertyName, inputArray) {
+      var seenDuplicate = false,
+          testObject = {};
+    
+      inputArray.map(function(item) {
+        var itemPropertyName = item[propertyName].toLowerCase();    
+        if (itemPropertyName.toLowerCase() in testObject) {
+          testObject[itemPropertyName].duplicate = true;
+          item.duplicate = true;
+          seenDuplicate = true;
+        }
+        else {
+          testObject[itemPropertyName] = item;
+          delete item.duplicate;
+        }
+      });
+    
+      return seenDuplicate;
+    }
+
+    this.removeSkill=function(index){
+      console.log(`now removing ${index}`)
+      this.Skills.splice(index,1);
+    }
+    this.addSkill=function(){
+      let count=this.Skills.length+1;
+      this.Skills.push({'skill':'skill '+count});
+    }
+this.Skills=[{'skill':'skill 1'},{'skill':'skill 2'},{'skill':'skill 3'}]
+    this.onSkillTxtBlur=function(){
+checkDuplicateInObject('skill',this.Skills);
+
+    }
   }
 
 }
